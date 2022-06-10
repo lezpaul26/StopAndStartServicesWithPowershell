3659f8d88a010c643179313d89877135b3cd16fc
Francisco Salas <32375423+lezpaul26@users.noreply.github.com>
2022-06-10 13:36:12 -0500

    Initial Commit

---
+++ StartServices.ps1
@@ -0,0 +1,27 @@
+﻿
+$str = "Service name1","Service name2","Service nameN"
+
+
+foreach ($LINE in $str) 
+{
+
+    Try
+    {
+       Get-Service $LINE | Star-Service   
+    }
+    Catch
+    {
+        
+        Write-Host "An error occurred trying stop: $LINE " -ForegroundColor Red        
+        Write-Host $_.Exception.Message
+    }
+    Finally
+    {
+        
+        Write-Host "finish trying start: $LINE "
+    }
+
+}
+
+
+Write-Host "finish trying stop servi1ces"
